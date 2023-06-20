787ba3eb8f27a790056880ed17b4d8eb7b8a4ffb
chaeunjoo <chaeunjoo@naver.com>
2023-06-20 22:32:33 +0900

    20230620 즐거운 자바 - 변수와 리터럴

---
+++ javaStudy/src/javaStudy/IntPrint.java
@@ -0,0 +1,34 @@
+package javaStudy;
+
+public class IntPrint {
+	public static void main(String[] args) {
+		
+		int i = 1;
+		// int : 정수 타입(type)을 나타내는 키워드(keyword)
+		// 키워드란 java 언어에서 정한 예약어
+		
+		// i : "변수 i"
+		// 변수는 하나의 값을 가질 수 있는 공간(메모리의 어떤 영역)
+		// i는 변수의 이름, 즉 변수명
+
+		// 1 : "정수 리터럴(literal) 1"
+		// 리터럴이란 변수에 입력되는 값
+		// 정수 타입 변수 i를 선언함과 동시에 1로 초기화
+		
+		// 정수 타입은 4byte의 메모리를 사용. 이 4byte 메모리에 숫자 1이 2진수로 저장
+		
+		// 정수 타입 변수를 선언할 때 초기값을 넣어주지 않으면 0
+		
+		// 기본형 타입 : byte, short, int, long, float, double, char, boolean
+		// 단어의 첫 글자가 모두 소문자
+		// 정해진 크기의 메모리를 확보하고, 그 메모리 안에 값을 저장
+		
+		// 참조형(레퍼런스 타입) : class, interface
+		// 단어의 첫 글자가 대문자
+		// 값을 가지지 않고, 값을 참조
+		
+		System.out.println(i);
+		System.out.println(100);
+		
+	}
+}
