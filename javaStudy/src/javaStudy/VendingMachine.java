2e745545a90ab015094f9b2ecad1a485d60aea09
chaeunjoo <eunjoocha@naver.com>
2023-06-23 23:43:48 +0900

    20230623 메소드

--- javaStudy/src/javaStudy/VendingMachine.java
+++ javaStudy/src/javaStudy/VendingMachine.java
@@ -2,8 +2,11 @@ package javaStudy;
 
 public class VendingMachine {
 	// 필드 : 가지는 것들
-	// 생성자 : 
+	// 생성자
 	// 메소드 : 기능 
-	
-	
+	public String pushProductButton(int menuId) {
+		System.out.println(menuId + "를 전달받았습니다. ");
+		return "콜라";
+	}
+	// VendingMachine에 추가된 메소드를 VendingMachineMain의 main메소드에서 레퍼런스 변수를 이용해 호출
 }
--- javaStudy/src/javaStudy/VendingMachineMain.java
+++ javaStudy/src/javaStudy/VendingMachineMain.java
@@ -3,8 +3,8 @@ package javaStudy;
 public class VendingMachineMain {
 	public static void main(String[] args) {
 	// static이 붙은 메소드는 클래스 메소드
-	// 클래스 메소드는 인스턴스를 생성하지 않다고 사용 가능
-	// 메모리에 올라가 있는 것만 사용 가능한데 클래스 메소드는 인스턴스를 생성하지 않다고 메모리에 올라간다.
+	// 클래스 메소드는 인스턴스를 생성하지 않고도 사용 가능
+	// 메모리에 올라가 있는 것만 사용 가능한데 클래스 메소드는 인스턴스를 생성하지 않고도 메모리에 올라간다.
 		
 	// 폰노이만의 프로그램 내장 방식
 	// 모든 프로그램은 메모리에 올라 가야지만 실행될 수 있다.
@@ -18,10 +18,10 @@ public class VendingMachineMain {
 		// 메모리 상에 VendingMachine이 생성
 		// vm1이라는 이름으로 가리키는 것
 		
-		// 메인 메소드가 동작하려면 VendingMachine가 필요
+		// 메인 메소드가 동작하려면 VendingMachine이 필요
 		// 해당 클래스가 main 메소드 안에서 사용되고 있기 때문이다.
 		// 메인 메소드에서 VendingMachine을 사용하는데, VendingMachine 클래스가 존재하지 않는다면 컴파일 오류가 발생
-		// VendingMachine 클래스가 있어야만 main 메소드는 컴파일되고 실해왼다.
+		// VendingMachine 클래스가 있어야만 main 메소드는 컴파일되고 실행된다.
 		// 그렇기 때문에 의존한다고 표현
 		
 		// VendingMachineMain 클래스는 VendingMachine 클래스에 의존
@@ -30,5 +30,13 @@ public class VendingMachineMain {
 	
 		// 클래스명 변수명　 = new 　　　 클래스명();
 		// 참조타입 참조면수 = new 연산자 생성자 
+		
+		String product = vm1.pushProductButton(100);
+		System.out.println(product);
+		
+		// 메뉴 아이디 100을 전달 받고, "콜라"가 리턴 
+		// 리턴된 "콜라"는 product 변수가 참조하게 된다.
+		// product의 값인 "콜라"가 출력되고 main 메소드가 종료되면서 자바 애플리케이션은 종료
+		
 	}
 }
