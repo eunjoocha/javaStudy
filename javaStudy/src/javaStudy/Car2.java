265fdcf0c180f4015d004d323b6809bd1f994f2e
chaeunjoo <eunjoocha@naver.com>
2023-07-07 23:28:32 +0900

    20230707

--- javaStudy/src/javaStudy/Car2.java
+++ javaStudy/src/javaStudy/Car2.java
@@ -17,4 +17,4 @@ public abstract class Car2 {
 //추상 클래스를 상속받는 자손이 인스턴스가 된다.
 //abstract 키워드를 사용하여 클래스를 정의한다.
 //추상 클래스는 보통 1개 이상의 추상 메소드를 가진다. (추상 메소드가 없어도 오류가 발생하지 않는다.)
-//public abstract class 클래스명 {...}
+//public abstract class 클래스명 { ...}
